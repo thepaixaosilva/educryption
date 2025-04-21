1b3194f61ba20458b9cb79c6e4ebe2e9a0d18068
Matheus Silva <matpaixaoo@outlook.com>
2025-04-21 00:05:08 -0300

    refactor: icon management
    The Icons.tsx file acts as both an adapter - because an icon can be replaced with little to no configuration - and a facade for the icons used in the app. It imports all the SVG icons from the assets/icons/svg directory and exports them as React components.

---
+++ assets/icons/Icons.tsx
@@ -0,0 +1,37 @@
+import ChkCircle from './svg/chk-circle.svg';
+import Chk from './svg/chk.svg';
+import CrossCircle from './svg/cross-circle.svg';
+import Cross from './svg/cross.svg';
+import Hide from './svg/hide.svg';
+import Home from './svg/home.svg';
+import ListPointers from './svg/list-pointers.svg';
+import LockOff from './svg/lock-off.svg';
+import LockOn from './svg/lock-on.svg';
+import Next from './svg/next.svg';
+import Pencil from './svg/pencil.svg';
+import Qr from './svg/qr.svg';
+import Show from './svg/show.svg';
+import ThreeDots from './svg/three-dots.svg';
+import UsrCirle from './svg/usr-circle.svg';
+import Usr from './svg/usr.svg';
+
+const Icons = {
+  ChkCircle,
+  Chk,
+  CrossCircle,
+  Cross,
+  Hide,
+  Home,
+  ListPointers,
+  LockOff,
+  LockOn,
+  Next,
+  Pencil,
+  Qr,
+  Show,
+  ThreeDots,
+  UsrCirle,
+  Usr,
+};
+
+export default Icons;
